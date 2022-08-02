75354bc1f6d56ae3e4a6a2dd739fa86a7fb8a1f7
Shellya Nur <95962010+shellyanat@users.noreply.github.com>
2022-08-02 22:32:43 +0700

    Update finalproject.py

--- finalproject.py
+++ finalproject.py
@@ -6,6 +6,7 @@ import numpy as np
 from PIL import Image
 from numerize import numerize
 from chart_studio import plotly
+plotly.sign_in(username='shellyanat', api_key='CHyax83b2yesSo6k1bUL')
 import plotly.graph_objects as go
 import plotly.express as px
 
@@ -13,7 +14,7 @@ import plotly.express as px
 st.set_page_config(layout="wide")
 st.title("Artis Korea sebagai Duta Merek Brand Lokal")
 
-plotly.sign_in(username='shellyanat', api_key='CHyax83b2yesSo6k1bUL')
+
 
 
 #data
